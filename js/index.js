d3072e2a9e23d67aa13ee036898572e999b24de8
Marco Kushta <marcokushta@gmail.com>
2023-12-04 09:21:48 -0600

    Testing web app

--- js/index.js
+++ js/index.js
@@ -10,3 +10,28 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+document.getElementById('pearson-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var x = document.getElementById('x').value;
+    var y = document.getElementById('y').value;
+    fetch('../py/pearson', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'x=' + encodeURIComponent(x) + '&y=' + encodeURIComponent(y),
+    })
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('HTTP error ' + response.status);
+        }
+        return response.json();
+    })
+    .then(function(results) {
+        document.getElementById('results').innerText = JSON.stringify(results);
+    })
+    .catch(function(error) {
+        document.getElementById('results').innerText = 'An error occurred: ' + error.message;
+    });
+});
